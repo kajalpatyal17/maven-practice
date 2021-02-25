69fab4173b955f66e9a7e8f2493d7a5e34c17b98
kajalpatyal17 <kajal.patyal@tothenew.com>
2021-02-25 20:47:28 +0530

    commit without .idea/ and target/

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    void dis(){
+        System.out.println("HELLO World");
+    }
+    public static void main(String[] args){
+        App obj =new App();
+        obj.dis();
+    }
+}
